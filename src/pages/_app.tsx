a58488ff0ba76ba8354f7d1ce617e0ed77b1244e
Igor Lourenço <theigorlourenco@gmail.com>
2020-12-22 18:59:08 -0300

    STYLE: foram adicionadas estilizações com Chakra UI e Absolute Imports (#6)
    * STYLE (CHAKRA-UI): foi adicionada e configurada a biblioteca de estilização Chakra UI
    
    * STYLE: foi configurada a funcionalidade Absolute Imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
-import '../styles/globals.css'
-import {ProvideAuth} from '../libraries/auth'
+import {CSSReset, ThemeProvider} from '@chakra-ui/react'
+import {ProvideAuth} from '@/libraries/auth'
+import theme from "@/styles/theme";
 
-function MyApp({ Component, pageProps }) {
-  return (
-      <ProvideAuth>
-        <Component {...pageProps} />
-      </ProvideAuth>
-  )
+function MyApp({Component, pageProps}) {
+    return (
+        <ThemeProvider theme={theme}>
+            <CSSReset/>
+            <ProvideAuth>
+                <Component {...pageProps} />
+            </ProvideAuth>
+        </ThemeProvider>
+    )
 }
 
 export default MyApp
