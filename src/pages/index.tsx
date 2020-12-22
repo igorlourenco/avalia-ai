231977fa7b0b78bef92d59d3de00f8f25c8fd860
Igor Lourenço <theigorlourenco@gmail.com>
2020-12-22 14:29:33 -0300

    FEAT (AUTENTICAÇÃO): foram adicionados os métodos de autenticação com Github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,37 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {useAuth} from '../libraries/auth'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Avalia aí!
-        </h1>
-      </main>
-    </div>
-  )
+    const auth = useAuth()
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Avalia aí!
+                </h1>
+
+                {
+                    !auth.user &&
+                    <button onClick={(e) => auth.signInWithGithub()}>entrar com github</button>
+                }
+
+                {
+                    auth.user &&
+                    <div>
+                        <h6>{auth.user.email}</h6>
+
+                        <button onClick={(e) => auth.signOut()}>SAIR</button>
+                    </div>
+                }
+            </main>
+        </div>
+    )
 }
