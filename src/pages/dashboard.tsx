cb8d45b32c13de837ada1edb5d2aa324c8772e3f
Igor Lourenço <theigorlourenco@gmail.com>
2021-01-06 14:58:24 -0300

    FIX: foi removido o <head> da página de Dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -6,21 +6,6 @@ import ProductTableSkeleton from "../components/ProductTableSkeleton";
 import ProductTable from "../components/ProductTable";
 import DashboardShell from "../components/DashboardShell";
 import ProductTableHeader from "../components/ProductTableHeader";
-import Head from "next/head";
-
-const DashboardHead = () => (
-    <Head>
-        <script
-            dangerouslySetInnerHTML={{
-                __html: `
-                        if(!document.cookie.includes('avalia-ai-auth')){
-                            window.location.href = "/"
-                        }
-                    `
-            }}
-        />
-    </Head>
-)
 
 const Dashboard = () => {
     const {user} = useAuth()
@@ -29,34 +14,26 @@ const Dashboard = () => {
 
     if (!data) {
         return (
-            <>
-                <DashboardHead/>
-                <DashboardShell title={`Meus Produtos`}>
-                    <ProductTableHeader isEmptyState={false}/>
-                    <ProductTableSkeleton/>
-                </DashboardShell>
-            </>
+            <DashboardShell title={`Meus Produtos`}>
+                <ProductTableHeader isEmptyState={false}/>
+                <ProductTableSkeleton/>
+            </DashboardShell>
         )
     }
 
     return data.products?.length > 0 ?
         (
-            <>
-                <DashboardHead/>
-                <DashboardShell title={`Meus Produtos`}>
-                    <ProductTableHeader isEmptyState={false}/>
-                    <ProductTable products={data.products}/>
-                </DashboardShell>
-            </>
+            <DashboardShell title={`Meus Produtos`}>
+                <ProductTableHeader isEmptyState={false}/>
+                <ProductTable products={data.products}/>
+            </DashboardShell>
+
         ) :
         (
-            <>
-                <DashboardHead/>
-                <DashboardShell title={`Meus Produtos`}>
-                    <ProductTableHeader isEmptyState={true}/>
-                    <EmptyState/>
-                </DashboardShell>
-            </>
+            <DashboardShell title={`Meus Produtos`}>
+                <ProductTableHeader isEmptyState={true}/>
+                <EmptyState/>
+            </DashboardShell>
         )
 
 }
